646705a959929fba1f1684e9599c545cdc053229
Adrian Schaedle <adrian@schaedle.me>
2016-12-08 16:01:22 -0500

    Remove node modules from repo

--- js/index.js
+++ js/index.js
@@ -1,5 +1,43 @@
-import Checkboxes from './Checkboxes.html';
+// import Checkboxes from "./Checkboxes.html";
+import Inferno from "inferno";
+import createElement from "inferno-create-element";
+import { emptyCanvas } from "./canvas"
 
-window.checkboxes = new Checkboxes({
-  target: document.querySelector('.js-Checkboxes')
-});
+const target = document.querySelector(".js-Checkboxes")
+
+// OLD IMPLEMENTATION
+// window.checkboxes = new Checkboxes({
+//   target: target
+// });
+
+function Checkbox (checked) {
+  return createElement("input", { type: "checkbox", checked })
+}
+
+function CheckboxCanvas (canvas) {
+  const checkboxCanvas = canvas.map((row) => createElement("div", null, row.map(Checkbox)))
+
+  return createElement("div", {
+    className: "CheckboxCanvasContainer"
+  }, checkboxCanvas)
+}
+
+function render () {
+  console.log("rendering")
+  const { width, height } = getCanvasSize()
+  const canvas = emptyCanvas(width, height)
+  Inferno.render(CheckboxCanvas(canvas), target)
+}
+
+function update () {
+  requestAnimationFrame(render)
+}
+
+function getCanvasSize () {
+  return {
+    width: Math.floor(window.innerWidth / 16),
+    height: Math.floor(window.innerHeight / 16) - 3
+  }
+}
+
+window.addEventListener("resize", update, false)
--- js/rollup.config.js
+++ js/rollup.config.js
@@ -1,9 +1,10 @@
-// rollup.config.js
 import svelte from 'rollup-plugin-svelte';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   entry: 'index.js',
   dest: 'bundle.js',
   format: 'iife',
-  plugins: [svelte()]
+  plugins: [svelte(), nodeResolve(), commonjs()]
 }
