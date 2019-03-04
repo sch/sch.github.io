d424385bdec0318aa2eb5314cdd2d7f5a0db596d
Adrian Schaedle <adrian@schaedle.me>
2019-03-03 20:34:44 -0500

    Upgrade rollup build deps
    Rollup is only used for the checkboxes feature right now, but I have
    a feeling I'm going to upgrade the webpack-related toolchain to use
    it. This updates to Rollup 1.x, and bumps all rollup plugins, except for
    the svelte one. New version of svelte has a bunch of breaking changes,
    so I'm going to do that separately.

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,22 @@
 import svelte from "rollup-plugin-svelte";
 import nodeResolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
-import uglify from "rollup-plugin-uglify";
+import { uglify } from "rollup-plugin-uglify";
 import buble from "rollup-plugin-buble";
-import filesize from "rollup-plugin-bundle-size"
+import filesize from "rollup-plugin-bundle-size";
 
-var plugins = [
-  svelte(),
-  nodeResolve(),
-  commonjs(),
-  buble()
-]
+var plugins = [svelte(), nodeResolve(), commonjs(), buble()];
 
 if (process.env.NODE_ENV === "production") {
-  plugins.push(uglify(), filesize())
+  plugins.push(uglify(), filesize());
 }
 
 export default {
-  entry: "js/index.js",
-  dest: "_site/js/bundle.js",
-  format: "iife",
-  sourceMap: true,
+  input: "js/index.js",
+  output: {
+    file: "_site/js/bundle.js",
+    format: "iife",
+    sourcemap: true
+  },
   plugins: plugins
-}
+};
